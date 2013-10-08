d8ac488b6a7eb8eb5056b235bc0c58d313e07e78
Ean Bowman <eanbowman@gmail.com>
2013-10-08 12:07:11 -0400

    Properly consuming the feed now.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,8 +4,7 @@ angular.module('eansDemoApp')
   .controller('MainCtrl', function ($scope,deviantArt) {
 				$scope.feedSrc="http://backend.deviantart.com/rss.xml?q=gallery%3Aeanbowman";
         $scope.feed = deviantArt.parseFeed($scope.feedSrc).then(function(response){
-            return response.data.responseData.feed;
+					if( typeof console != 'undefined' ) console.log( response.data.responseData.feed );
+          return response.data.responseData.feed;
         });
-
-
-  });
+	});
